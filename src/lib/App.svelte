2158dbd7a2e8ad278e034e38d052d19d287a3db7
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-04-24 14:50:08 -0600

    start

---
+++ src/lib/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from "svelte";
+    // may want to move this class into its own script but this will do for now.
+    export class ApparelDocument {
+        svg = document.createElement("svg")
+
+        constructor(name = "Untitled Document", width = 0, height = 0) {
+            this.name = $state(name)
+            this.width = $state(width)
+            this.height = $state(height)
+            documents.push(this)
+        }
+    }
+
+	/**
+     * @type {ApparelDocument[]}
+     */
+	let documents = $state([])
+    /**
+     * @type {ApparelDocument?}
+     */
+    let activeDocument = $state(null)
+
+    onMount(() => {
+        activeDocument = new ApparelDocument()
+    });
+</script>
---
+++ src/lib/DocumentEditor.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { Tool } from './tools/Tool'
+    import { BoxSelectTool } from './tools/BoxSelectTool/BoxSelectTool'
+    /**
+     * @type {(Tool[] | Tool)[]}
+     */
+    const tools = [
+        [
+            new BoxSelectTool()
+        ]
+    ]
+    /**
+     * @type {Tool?}
+     */
+    let currentTool = $state(null)
+    let style = $state({
+        fill: {
+            color: "#734b06ff" // placeholder default color
+        },
+        stroke: {
+            color: "#000000ff",
+            width: 4
+            // TODO: dotted line stuff idk
+        },
+        shape: {
+            opacity: 1,
+            blur: 0,
+            // todo: yes
+        }
+    }) 
+    /**
+     * @type {Node[]}
+     */
+    let selectedNodes = $state([])
+
+    let { svg } = $props()
+</script>
