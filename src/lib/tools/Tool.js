f8125e27496e827e4290b3868922ead406b33096
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-04-26 09:18:54 -0600

    Update Tool.js

--- src/lib/tools/Tool.js
+++ src/lib/tools/Tool.js
@@ -2,4 +2,34 @@ export class Tool {
 	name = 'Tool'
 	constructor() {
 	}
+
+	/**
+	 * When a click is started, node will be non-null if a shape was clicked.
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {Node?} topLevelNode
+	 * @param {Node?} bottomLevelNode
+	 */
+	onClickStart(x, y, topLevelNode, bottomLevelNode) {
+		
+	}
+
+	/**
+	 * When a click is ended, node will be non-null if a shape was clicked.
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {Node?} topLevelNode
+	 * @param {Node?} bottomLevelNode
+	 */
+	onClickEnd(x, y, topLevelNode, bottomLevelNode) {
+		
+	}
+
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 */
+	onMouseMoved(x, y) {
+		
+	}
 }
