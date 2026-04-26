8978e66fbfa99371e2c50c13fad4e6cc489953d4
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-04-26 12:05:17 -0600

    Update ArrowTool.js

--- src/lib/tools/ArrowTool.js
+++ src/lib/tools/ArrowTool.js
@@ -2,6 +2,8 @@ import { DraggedTool } from './DraggedTool'
 
 export class ArrowTool extends DraggedTool {
 	name = 'Arrow'
+	
+	// Tool Settings
 	thickness = 1
 	headThickness = 2
 	headLength = 2
